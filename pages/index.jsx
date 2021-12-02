7ba2d1d4accafe510eebbfef385e63db964413c9
Brent de Lara <brent.delara@care.com>
2021-12-01 21:10:27 -0700

    creating stub project

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,44 +1,30 @@
-import { useState, useEffect } from 'react';
-import produce from 'immer';
-
-const Notes = props => props.data.map(note => <div>{note.text}</div>);
+import { useState } from 'react';
 
 export default () => {
-  const initialData = [{ text: 'Loading Notes ... ' }];
+  const initialData = [];
   const [data, setData] = useState(initialData);
 
   const handleClick = () => {
     const text = document.querySelector('#noteinput').value.trim();
-    if (text) {
-      const nextState = produce(data, draftState => {
-        draftState.push({ text });
-      });
-      document.querySelector('#noteinput').value = '';
-
-      if (typeof window !== 'undefined') {
-        localStorage.setItem('data', JSON.stringify(nextState));
-      }
-
-      setData(nextState);
-    }
+    // TODO
   };
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const getData = localStorage.getItem('data');
-
-      if (getData !== '' && getData !== null) {
-        return setData(JSON.parse(getData));
-      }
-      return setData([]);
-    }
-  }, 0);
 
   return (
     <>
-      <input id="noteinput" style={{ width: '80%' }} type="text" placeholder="Enter a new note" />
-      <button onClick={() => handleClick()}>Add note</button>
-      <Notes data={data} />
+      <div>
+      <button onClick={() => { void(0); }}>All</button>
+      <button onClick={() => { void(0); }}>Active</button>
+      <button onClick={() => { void(0); }}>Completed</button>
+      </div>
+      <div>
+        <input id="noteinput" style={{ width: '40%' }} type="text" placeholder="Enter a new note" />
+        <input id="statusinput" style={{ width: '40%' }} type="text" placeholder="status" />
+        <button onClick={() => { void(0); }}>Add note</button>
+        <div>
+          {/* notes list */}
+        </div>
+      </div>
     </>
   );
 };
+ 
